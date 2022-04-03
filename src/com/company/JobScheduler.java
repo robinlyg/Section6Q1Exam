1c73695d735771429c74f905f6855a3028e03213
Robin Gralton <rgralton24@gmail.com>
2022-04-03 16:52:13 -0700

    Finished

--- src/com/company/JobScheduler.java
+++ src/com/company/JobScheduler.java
@@ -1,11 +1,5 @@
 package com.company;
 
-import java.awt.*;
-import java.util.AbstractQueue;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class JobScheduler  {
 // To ensure that all applications in Operating System have equal CPU time a
 //com.company.JobScheduler is needed. The job scheduler needs to maintain a list of jobs, and work
@@ -15,24 +9,24 @@ public class JobScheduler  {
 //Also, implement the following 3 methods and a constructor in legal Java code to
 //your ability
 
-    //if i use a dll as my
-    //data structure to use
 
-    private LinkedList<Process> jobQueue = new LinkedList();
-    //private int size = 0;
+    private CircularArrayQueue<Process> jobQueue;
+
 
     //constructor
     public JobScheduler(){
-        jobQueue = new LinkedList<>();
+        jobQueue = new CircularArrayQueue<>();
     }
 
+    public int getSize(){
+        return jobQueue.size();
+    }
 
-    public int getSize(){return jobQueue.size();}
     /* Adds a new job to the system.*/
     public void addProcess(Process p)
     {
         jobQueue.offer(p);
-        //size++;
+
     }
 
     /* Gets the next job and adds this job back to the
@@ -42,7 +36,7 @@ system, if no other jobs exist this job is sent back.*/
         if(jobQueue.peek() == null)
             return p;
         //adds this to back of system
-        jobQueue.offerLast(p);
+        jobQueue.offer(p);
         //gets next job
         return jobQueue.poll();
 
